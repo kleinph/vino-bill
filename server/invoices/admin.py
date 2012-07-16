293673e7bb63ce4061608d51d2ab1f95f182c842
Philipp Klein <philippklein@gmx.at>
2012-07-16 15:23:41 +0200

    added basic functionallity

---
+++ server/invoices/admin.py
@@ -0,0 +1,23 @@
+from invoices.models import Invoice, InvoicePosition, Wine, Category
+from django.contrib import admin
+from django import forms
+
+class InvoicePositionInline(admin.TabularInline):
+	model = InvoicePosition
+	readonly_fields = ["sum"]
+	
+class InvoiceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Allgemein", {"fields" : [("date", "rebate"), "customer"]})
+    ]
+    inlines = [InvoicePositionInline]
+    search_fields = ["customer"]
+    date_hierarchy = "date"
+    list_display = ("id", "date",)
+
+class WineAdmin(admin.ModelAdmin):
+    list_filter = ["category"]
+
+admin.site.register(Invoice, InvoiceAdmin)
+admin.site.register(Wine, WineAdmin)
+admin.site.register(Category)
--- server/invoices/models.py
+++ server/invoices/models.py
@@ -1,37 +1,72 @@
+# -*- coding: utf8 -*-
+
 from django.db import models
+from decimal import Decimal
 
-class Invoice(models.Model):
-    date = models.DateField(auto_now_add = True, verbose_name = "Datum")
-    customer = models.TextField("Kundendaten")
-    rebate = models.PositiveIntegerField("Rabatt")
-    invoice_postions = models.ManyToMany(InvoicePosition, verbose_name = "Rechnungsposten")
+class CurrencyField(models.DecimalField):
+    __metaclass__ = models.SubfieldBase
+
+    def to_python(self, value):
+        try:
+           return super(CurrencyField, self).to_python(value).quantize(Decimal("0.01"))
+        except AttributeError:
+           return None
+
+class Category(models.Model):
+    name = models.CharField(max_length = 20)
     
     class Meta:
-        verbose_name = "Rechnung"
-        verbose_name_plural = "Rechnungen"
-        
-    def __unicode__(self):
-        return self.name
-
-class InvoicePosition(models.Model):
-    quantity = models.PositiveIntegerField("Anzahl")
-    wine = models.OneToMany(Wine, verbose_name = "Wein")
-    
-    class Meta:
-        verbose_name = "Rechnungsposten"
-        verbose_name_plural = "Rechnungsposten"
+        verbose_name = "Kategorie"
+        verbose_name_plural = "Kategorien"
     
     def __unicode__(self):
         return self.name
 
 class Wine(models.Model):
-    description = models.CharField(max_length = 20, verbose_name = "Bezeichnung")
-    quantity = models.
-    price = models.
+    description = models.CharField("Bezeichnung", max_length = 30)
+    quantity = models.PositiveIntegerField("Gebinde (ml)")
+    price = CurrencyField("Preis", decimal_places = 2, max_digits = 5)
+    category = models.ForeignKey(Category, null = True, verbose_name = "Kategorie")
     
     class Meta:
         verbose_name = "Wein"
         verbose_name_plural = "Weine"
         
     def __unicode__(self):
-        return self.name
+        return self.description
+
+class Invoice(models.Model):
+    date = models.DateField("Datum")
+    customer = models.TextField("Kundendaten", blank = True)
+    rebate = models.PositiveIntegerField("Rabatt (%)")
+    invoice_positions = models.ManyToManyField(Wine, through = "InvoicePosition", verbose_name = "Rechnungsposten")
+    
+    class Meta:
+        verbose_name = "Rechnung"
+        verbose_name_plural = "Rechnungen"
+
+	#def _get_total(self):
+	#	sum = 0
+	#	for pos in self.invoice_positions.all()
+	#		sum += pos.sum_
+	#	return sum
+	#total = property(_get_sum)
+		
+	def __unicode__(self):
+		return unicode(self.id)
+
+class InvoicePosition(models.Model):
+    quantity = models.IntegerField("Anzahl")
+    wine = models.ForeignKey(Wine, verbose_name = "Wein")
+    invoice = models.ForeignKey(Invoice)
+    
+    def _get_sum(self):
+        return self.wine.price * self.quantity
+    sum = property(_get_sum)
+    
+    class Meta:
+        verbose_name = "Rechnungsposten"
+        verbose_name_plural = "Rechnungsposten"
+    
+    def __unicode__(self):
+        return u"{quantity} x {name} - € {sum}".format(quantity = self.quantity, name = self.wine, sum = self.sum)

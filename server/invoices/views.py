f1bfa78268df5e084b2c103c3886d88c18920a1f
Philipp Klein <philippklein@gmx.at>
2012-07-30 21:54:33 +0200

    added a index and a submit view

--- server/invoices/views.py
+++ server/invoices/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from invoices.models import Invoice, Wine, Category, InvoicePosition
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+    category_list = Category.objects.all()
+    wine_list = Wine.objects.all()
+    return render_to_response("index.html", {"categories": category_list, "wines": wine_list}, 
+        context_instance = RequestContext(request))
+    
+def submit(request):
+    return HttpResponse("Submit")

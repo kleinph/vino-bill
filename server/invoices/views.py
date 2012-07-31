de72c6a9d5dc35f29660bfeb71eb0f5936897776
Philipp Klein <philippklein@gmx.at>
2012-07-31 22:32:59 +0200

    made the wine-list sort alphabetical

--- server/invoices/views.py
+++ server/invoices/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response
 
 def index(request):
     category_list = Category.objects.all()
-    wine_list = Wine.objects.all()
+    wine_list = Wine.objects.order_by("description")
     return render_to_response("index.html", {"categories": category_list, "wines": wine_list}, 
         context_instance = RequestContext(request))
     

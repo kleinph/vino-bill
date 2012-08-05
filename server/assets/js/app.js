96d75bbf0f76020ea62badce5cdd249e706d43f4
Philipp Klein <philippklein@gmx.at>
2012-08-05 21:50:10 +0200

    reworked the js to bring in more oop and did some minor improvements

--- server/assets/js/app.js
+++ server/assets/js/app.js
@@ -1,42 +1,95 @@
-var pos = 0;
-
-function updateTotal() {
-	var total = 0;
+function Invoice() {
+	var self = this;
+	var itemCount = 0;
 	
-	$("#wine-table .sum").each(function(index, object) {
-		value = 
-		total += Number($(this).html().replace(',', '.'));
-	});
+	this.rebate = 0;
+	this.customerData;
+	this.items = {};
 	
-	$("#wine-table #total .total").html("€ " + total.toFixed(2).replace('.', ','));
+	this.addItem = function(item) {
+		if (!this.items[item.id]) {
+			item.pos = ++itemCount;
+			this.items[item.id] = item;
+			this.items[item.id].updateTable();
+			log.info("added item " + item.id + " (" + item.name + ")");
+		} else {
+			if (item.quantity == 0) {
+				$("#wine-table #item-" + item.id).remove();
+				--itemCount;
+				updateIndices(this.items[item.id].pos);
+				delete this.items[item.id];
+				log.info("deletd " + item.id + " (" + item.name + ")");
+			} else {
+				this.items[item.id].quantity = item.quantity;
+				this.items[item.id].updateTable();
+				log.info("updated item #" + item.pos + " (" + item.name + ")");
+			}
+		}
+		
+		calculateTotal();
+	}
+	
+	function setRebate(rebate) { }
+	
+	function calculateTotal() {
+		var total = 0;
+		
+		for (id in self.items) {
+			total += self.items[id].price * self.items[id].quantity;
+		}
+		
+		$("#wine-table #total .total").html("€ " + total.toFixed(2).replace('.', ','));
+	}
+	
+	function updateIndices(startIndex) {
+		for (id in self.items) {
+			if (self.items[id].pos > startIndex) {
+				--self.items[id].pos;
+				self.items[id].updateTable();
+			}
+		}
+	}
+	
 }
+
+function InvoiceItem(id, name, quantity, price) {
+	this.pos;
+	this.id = id;
+	this.name = name;
+	this.quantity = quantity;
+	this.price = price;
+	
+	this.updateTable = function() {
+		var sum = (this.quantity * this.price).toFixed(2).replace('.', ',');
+		
+		if ($("#wine-table #item-" + this.id).length == 0) {
+			$("#wine-table #total").before(
+				"<tr id='item-" + this.id + "'>" +
+				"  <td class='pos'>" + this.pos + "</td>" +
+				"  <td>" + this.name + "</td>" +
+				"  <td class='right'>€ " + this.price.replace('.', ',') + "</td>" +
+				"  <td class='right quantity'>" + this.quantity + "</td>" +
+				"  <td class='right'>€ <span class='sum'>" + sum + "</span></td>" +
+				"</tr>"
+			);
+		} else {
+			$("#wine-table #item-" + this.id + " .pos").html(this.pos);
+			$("#wine-table #item-" + this.id + " .quantity").html(this.quantity);
+			$("#wine-table #item-" + this.id + " .sum").html(sum);
+		}
+	}
+}
+
+var invoice = new Invoice();
 
 function updateTable(field) {
 	id = getId(field)
 	name = $("#wine-" + id + " .description").html();
 	quantity = $("#quantity-" + id).val();
 	price = $("#wine-" + id + " .price").html().replace(',', '.');
-	sum = (quantity * price).toFixed(2).replace('.', ',');
 	
-	if ($("#wine-table #item-" + id).length == 0) {
-		$("#wine-table #total").before(
-			"<tr id='item-" + id + "'>" +
-			"  <td>" + ++pos + "</td>" +
-			"  <td>" + name + "</td>" +
-			"  <td class='right'>€ " + price.replace('.', ',') + "</td>" +
-			"  <td class='right quantity'>" + quantity + "</td>" +
-			"  <td class='right'>€ <span class='sum'>" + sum + "</span></td>" +
-			"</tr>"
-		);
-	} else if (quantity == 0) {
-		$("#wine-table #item-" + id).remove();
-		--pos;
-	} else {
-		$("#wine-table #item-" + id + " .quantity").html(quantity);
-		$("#wine-table #item-" + id + " .sum").html(sum);
-	}
-	
-	updateTotal();
+	item = new InvoiceItem(id, name, quantity, price);
+	invoice.addItem(item);
 }
 
 function decrease(field) {

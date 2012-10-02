254e45c831198cf3829ec0c3f5f9c28a60ac8dd3
Philipp Klein <philippklein@gmx.at>
2012-10-03 00:35:29 +0200

    add angular.js MVC framework and a view for that in /angular

--- server/invoices/urls.py
+++ server/invoices/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns("invoices.views",
     url(r"^$", "index"),
+    url(r"angular$", "angular"),
 )
--- server/invoices/views.py
+++ server/invoices/views.py
@@ -1,7 +1,5 @@
 from invoices.models import Invoice, Wine, Category, InvoicePosition
-from django.http import QueryDict
-from django.http import HttpRequest
-from django.http import HttpResponse
+from django.http import QueryDict, HttpRequest, HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from datetime import datetime
@@ -11,6 +9,9 @@ def index(request):
     wine_list = Wine.objects.order_by("description")
     return render_to_response("index.html", {"categories": category_list, "wines": wine_list}, 
         context_instance = RequestContext(request))
+    
+def angular(request):
+    return HttpResponse(open("templates/angular.html").read())
     
 def submit(request):
     rebate = request.POST["rebate"]

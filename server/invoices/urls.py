df06368fea2f0545e246f86caf220d0efbb82d48
Philipp Klein <philippklein@gmx.at>
2012-10-15 13:25:09 +0200

    remove unneeded files and use the angular page as index page

--- server/invoices/urls.py
+++ server/invoices/urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns("invoices.views",
     url(r"^$", "index"),
-    url(r"angular$", "angular"),
 )
--- server/invoices/views.py
+++ server/invoices/views.py
@@ -5,13 +5,7 @@ from django.shortcuts import render_to_response
 from datetime import datetime
 
 def index(request):
-    category_list = Category.objects.all()
-    wine_list = Wine.objects.order_by("description")
-    return render_to_response("index.html", {"categories": category_list, "wines": wine_list}, 
-        context_instance = RequestContext(request))
-    
-def angular(request):
-    return HttpResponse(open("templates/angular.html").read())
+    return HttpResponse(open("templates/index.html").read())
     
 def submit(request):
     rebate = request.POST["rebate"]

fc3bc677c8420e5a44e2ce797d2971571a3f6512
Philipp Klein <philippklein@gmx.at>
2012-10-16 10:26:47 +0200

    improve create behaviour of REST api
    (now you can create related resources in one api call)

--- server/api/api.py
+++ server/api/api.py
@@ -5,22 +5,22 @@ from invoices.models import Category, Wine, Invoice, InvoicePosition
 from pretty_json_serializer import PrettyJSONSerializer
 
 class WineResource(ModelResource):
-    category = fields.ToOneField("api.api.CategoryResource", "category", null = True)
+    category = fields.ToOneField("api.api.CategoryResource", "category", related_name = "wines", null = True)
     
     class Meta:
         queryset = Wine.objects.all()
         serializer = PrettyJSONSerializer()
         
 class CategoryResource(ModelResource):
-    wines = fields.ToManyField(WineResource, "wine_set", full = True)
+    wines = fields.ToManyField(WineResource, "wine_set", related_name = "category", full = True)
 
     class Meta:
         queryset = Category.objects.all()
         serializer = PrettyJSONSerializer()
 
 class InvoicePositionResource(ModelResource):
+    invoice = fields.ToOneField("api.api.InvoiceResource", "invoice", related_name = "items")
     wine = fields.ToOneField(WineResource, "wine")
-    invoice = fields.ToOneField("api.api.InvoiceResource", "invoice")
     
     class Meta:
         queryset = InvoicePosition.objects.all()
@@ -28,7 +28,7 @@ class InvoicePositionResource(ModelResource):
         serializer = PrettyJSONSerializer()
         
 class InvoiceResource(ModelResource):
-    items = fields.ToManyField(InvoicePositionResource, "invoiceposition_set")
+    items = fields.ToManyField(InvoicePositionResource, "invoiceposition_set", related_name = "invoice")
     
     class Meta:
         queryset = Invoice.objects.all()

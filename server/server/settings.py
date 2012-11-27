b15fb098d2e9c03fb9f65f7f8f21511ebf6a1a88
Philipp Klein <philipptheklein@gmail.com>
2012-11-27 01:21:10 +0100

    Add some application specific settings into settings.py

--- server/server/settings.py
+++ server/server/settings.py
@@ -84,6 +84,21 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
+# Absolute path to directory where invoices should be stored
+INVOICE_DIR = '/usr/share/vinoBill/invoices/'
+
+# Prefix for invoice files.
+# Invoice filename will be: prefix+invoice-id+.pdf
+# e.g.: invoice-10.pdf (with prefix = 'invoice-')
+INVOICE_PREFIX = ''
+
+# Prefix for the invoice ID
+# e.g.: AB42
+INVOICE_ID_PREFIX = 'AH'
+
+# Value of the local sales tax (Umsatzsteuer) in percent
+UST = 16
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '^=cmtfchxmjn+d#^bx2f*x+(pmr7!2rpoccz=4lrs#p7pk#ev='
 

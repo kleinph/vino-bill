e9c3eb6d499a9a461975bae69ee1013d1bf1d5ff
Philipp Klein <philipptheklein@gmail.com>
2012-11-29 21:23:13 +0100

    Add urls for pdf generation and printing

--- server/invoices/urls.py
+++ server/invoices/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns("invoices.views",
+    url(r"^pdf/(\d+)", "pdf"),
+    url(r"^print/(\d+)", "print_invoice"),
     url(r"^$", "index"),
 )

a9a731985d65ef57b22c46dc2b79283f43e7cb95
Philipp Klein <philipptheklein@gmail.com>
2014-07-19 16:01:23 +0200

    Fix deprecation warning for simplejson

--- server/api/pretty_json_serializer.py
+++ server/api/pretty_json_serializer.py
@@ -1,5 +1,5 @@
-from django.core.serializers import json
-from django.utils import simplejson
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 from tastypie.serializers import Serializer
 
 class PrettyJSONSerializer(Serializer):
@@ -8,5 +8,5 @@ class PrettyJSONSerializer(Serializer):
     def to_json(self, data, options=None):
         options = options or {}
         data = self.to_simple(data, options)
-        return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
+        return json.dumps(data, cls=DjangoJSONEncoder,
                 sort_keys=True, ensure_ascii=False, indent=self.json_indent)
